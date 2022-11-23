414d8f3f763b5920619c1759a0cc011ecbbbde02
FortuneCodebox <fortunecodebox@Fortunes-MacBook-Pro.local>
2022-11-23 18:25:15 -0500

    Test Create Tag Model & Tag Model implementation & relationship with Recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(**params):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(**params)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -66,3 +71,9 @@ class ModelTests(TestCase):
             description='Sample recipe description'
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test Creating a tag is successful"""
+        user = create_user(email='user123@example.com', password='test123')
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        self.assertEqual(str(tag), tag.name)
